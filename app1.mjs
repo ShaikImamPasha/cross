4a182dddca74d8a4de566b4e17fe87dcf9a2376d
sk imam pashga <122773374+ShaikImamPasha@users.noreply.github.com>
2023-12-16 03:01:59 -0800

    Add files via upload

---
+++ app1.mjs
@@ -0,0 +1,29 @@
+// app.mjs
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+// Define the CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+// Define the Swiggy API proxy route
+app.use('/api/proxy/swiggy', createProxyMiddleware({
+  target: 'https://www.swiggy.com',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api/proxy/swiggy': '/dapi',
+  },
+}));
+
+app.listen(PORT, () => {
+  console.log('Server is running on port', PORT);
+});
